f18b07f6c5672f238861a783c03e5ee76458c532
Vinicius Spada Melo <vinicius.smelo54@gmail.com>
2021-12-17 19:47:58 -0300

    feat: fix generateCertificate & add permissions and env variables in yml file

--- src/utils/userAlreadyExists.ts
+++ src/utils/userAlreadyExists.ts
@@ -1,11 +1,7 @@
 import { getUserById } from "./getUserById";
 
 export const userAlreadyExists = async (id: string) => {
-  const response = await getUserById(id);
+  const user = await getUserById(id);
 
-  if (!response) return false;
-
-  const userAlreadyExists = response.Items[0];
-
-  return userAlreadyExists;
+  return !!user;
 };
